4d98a5e5109b906f71c0f0cf35f50fed44d00dad
Kristofer <145542649+kiku69@users.noreply.github.com>
2024-10-11 11:40:14 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,119 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+
 <template>
   <div class="container">
-    <div class="box big">Box 1</div>
-    <div class="box">Box 2</div>
-    <div class="box">Box 3</div>
-    <div class="box big">Box 4</div>
-    <div class="box">Box 5</div>
-    <div class="box">Box 6</div>
+    <div class="row">
+      <div class="box box-1">
+        <h6>Resilience</h6>
+      </div>
+      <div class="box box-2">
+        <h6>Growth</h6>
+      </div>
+      <div class="box box-3">
+        <h3>Engagement</h3> 
+      </div>
+    </div>
+    <div class="row">
+      <div class="box box-4">
+        <h3>Visioning </h3>
+      </div>
+      <div class="box box-5">
+        <h6>Goal Orientation</h6>
+      </div>
+      <div class="box box-6">
+        <h6>Self heliief</h6>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'BoxLayout'
-}
+<script setup>
 </script>
 
-<style>
+<style scoped>
 .container {
   display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
+  flex-direction: column;
+  gap: 8px;
 }
 
-.box {
-  background-color: lightblue;
-  border: 1px solid #007BFF;
-  padding: 20px;
-  text-align: center;
-  flex: 1 1 30%; /* Default size for small boxes */
+.row {
+  display: flex;
+  gap: 8px;
 }
 
-.big {
-  flex: 1 1 45%; /* Bigger size for larger boxes */
+.box-1 {
+  background-color: #e63e3e;
+  width: 100px;
+  height: 100px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  border-radius: 5px;
+}
+
+.box-2 {
+  background-color: #1fab89;
+  width: 100px;
+  height: 100px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  border-radius: 5px;
+}
+
+.box-3 {
+  background-color: #d17219;
+  width: 210px;
+  height: 210px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  border-radius: 5px;
+}
+
+.box-4 {
+  background-color: #a168f1;
+  width: 210px;
+  height: 210px;
+  margin-top: -110px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  border-radius: 5px;
+}
+
+.box-5 {
+  background-color: #1c7eff;
+  width: 100px;
+  height: 100px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  border-radius: 5px;
+}
+
+.box-6 {
+  background-color: #a200ff;
+  width: 100px;
+  height: 100px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  border-radius: 5px;
 }
 </style>
-
